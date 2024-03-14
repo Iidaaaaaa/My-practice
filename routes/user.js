c1d64da14367464aa86c74baa896f2e1c0ad2990
Iidaaaaaa <iidaaaatomo27@gmail.com>
2024-03-15 00:25:59 +0900

    paramsの使い方

--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,8 @@ router.get("/", (req, res) => {
 router.get("/info", (req, res) => {
   res.send("ユーザー情報です");
 });
+router.get("/:id", (req, res) => {
+  res.send(`${req.params.id}のユーザー情報を取得しました。`);
+});
 
 module.exports = router;
