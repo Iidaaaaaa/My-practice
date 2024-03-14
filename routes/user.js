e144e61ee90db974d1e9e7147d41fa133f66cb9e
Iidaaaaaa <iidaaaatomo27@gmail.com>
2024-03-15 00:26:31 +0900

    post,delete

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,12 @@ router.get("/", (req, res) => {
 router.get("/info", (req, res) => {
   res.send("ユーザー情報です");
 });
+router.post("/:id", (req, res) => {
+  res.send(`${req.params.id}のユーザー情報を取得しました。`);
+});
+router.delete("/:id", (req, res) => {
+  res.send(`${req.params.id}のユーザー情報を取得しました。`);
+});
 router.get("/:id", (req, res) => {
   res.send(`${req.params.id}のユーザー情報を取得しました。`);
 });
