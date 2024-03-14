da3a2ecd7ae8df52b85f15fda49a5adcde6b1b25
Iidaaaaaa <iidaaaatomo27@gmail.com>
2024-03-15 00:37:21 +0900

    views

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const app = express();
 const PORT = 3000;
 const userRouter = require("./routes/user");
 
+// app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   //   console.log("hello express");
   //   res.send("<h1>こんにちは</h1>");
   //   res.sendStatus(401);
   //   res.status(500).send("エラーです");
-  res.status(500).json({ msg: "エラーです" });
+  //   res.status(500).json({ msg: "エラーです" });
+  res.render("index", { text: "NodejsとExpress" });
 });
 
 app.get("/user", (req, res) => {
