55e185b77b8330618286cde30cea3635c4fca6cc
Iidaaaaaa <iidaaaatomo27@gmail.com>
2024-03-15 00:14:48 +0900

    res.send

--- server.js
+++ server.js
@@ -4,4 +4,11 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  //   console.log("hello express");
+  //   res.send("<h1>こんにちは</h1>");
+  //   res.sendStatus(401);
+  //   res.status(500).send("エラーです");
+});
+
 app.listen(PORT, () => console.log("サーバー起動"));
